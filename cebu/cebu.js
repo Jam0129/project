ae69a8a10e3848352d7b2e8349d7464d8badd47a
SJM <sterry9160@gmail.com>
2025-03-04 23:13:50 +0900

    아직 덜 최종

--- cebu/cebu.js
+++ cebu/cebu.js
@@ -7,8 +7,13 @@ $(document).ready(function () {
                 "height": "55vh",
                 "transition": "all 0.3s ease-in-out"
             });
-            $(this).find(".down, .bottom , .circle").fadeOut(100);
+            $(this).find(".down, .bottom").fadeOut(100);
             $(this).find(".middle").fadeIn(700);
+
+            $(this).find(".up").css({
+                "width": $(this).find(".up").width(), 
+                "height": $(this).find(".up").height()
+            });
 
         },
         function () {
@@ -17,8 +22,13 @@ $(document).ready(function () {
                 "height": "55vh",
                 "transition": "all 0.3s ease-in-out"
             });
-            $(this).find(".down, .bottom, .circle").fadeIn(100);
+            $(this).find(".down, .bottom").fadeIn(100);
             $(this).find(".middle").fadeOut(100);
+
+            $(this).find(".up").css({
+                "width": $(this).find(".up").width(),
+                "height": $(this).find(".up").height()
+            });
 
         }
     );
