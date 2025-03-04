0282c49b4044a2cd83e540c0dca401b1989c8e81
SJM <sterry9160@gmail.com>
2025-03-05 05:43:25 +0900

    시작

---
+++ sterry96/src/main/java/com/portfolio/sterry96/Sterry96Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.sterry96;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sterry96Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sterry96Application.class, args);
+	}
+
+}
---
+++ sterry96/src/test/java/com/portfolio/sterry96/Sterry96ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.sterry96;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sterry96ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
